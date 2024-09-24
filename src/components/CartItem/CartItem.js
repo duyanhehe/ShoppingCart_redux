9fa728cfbe7cbef19f428c10f62d66eee9b9745f
duyanhehe <duyanhex@gmail.com>
2024-09-24 14:32:09 +0700

    add cart, change quantity of the items

---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import { DeleteOutlined } from "@ant-design/icons";
+import { Button, InputNumber } from "antd";
+import { useDispatch } from "react-redux";
+import { updateQuantity } from "../redux/slice/cartSlice";
+
+function CartItem({ data }) {
+  const dispatch = useDispatch();
+
+  console.log(data);
+  const [quantity, setQuantity] = useState(data?.quantity);
+  const [totalPrice, setTotalPrice] = useState(+data?.price * +data?.quantity);
+
+  const handleChange = (value) => {
+    const validValue = value > 0 ? value : 1;
+    setQuantity(validValue);
+  };
+
+  useEffect(() => {
+    setTotalPrice(data?.price * quantity);
+    dispatch(updateQuantity({ id: data?.id, quantity }));
+  }, [quantity, data?.price, data?.id, dispatch]);
+
+  return (
+    <tr>
+      <td>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img
+            src={data.imageUrl}
+            alt={data.name}
+            style={{ width: "50px", height: "50px", marginRight: "10px" }}
+          />
+          <p style={{ margin: 0 }}>{data.name}</p>
+        </div>
+      </td>
+      <td>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {data.price}$
+        </div>
+      </td>
+      <td>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <InputNumber min={1} value={quantity} onChange={handleChange} />
+        </div>
+      </td>
+      <td>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          {totalPrice}$
+        </div>
+      </td>
+      <td>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button color="danger" variant="solid">
+            <DeleteOutlined />
+          </Button>
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+export default CartItem;
--- src/components/Page/Cart.js
+++ src/components/Page/Cart.js
@@ -1,9 +1,38 @@
+import { Row } from "antd";
 import { useSelector } from "react-redux";
+import CartItem from "../CartItem/CartItem";
 
-function Cart() {
+export default function Cart() {
   const cart = useSelector((state) => state.cart);
   console.log(cart);
-  return <div>Cart</div>;
-}
 
-export default Cart;
+  return (
+    <Row>
+      {cart?.list && cart?.list.length > 0 ? (
+        <table style={{ width: "100%" }}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody style={{ verticalAlign: "middle" }}>
+            {cart?.list.map((item) => {
+              return <CartItem key={item?.id} data={item} />;
+            })}
+            <tr style={{ fontSize: 20, textAlign: "right" }}>
+              <td colSpan={3}>Total:</td>
+              <td colSpan={1}>{cart?.total}$</td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <h1>Empty</h1>
+      )}
+    </Row>
+  );
+}
